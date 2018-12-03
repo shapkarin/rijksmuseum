e21678751cf35e17cc8b781400b34945b549f6fb
Yury Shapkarin <yu.shapkarin@gmail.com>
2018-12-04 01:32:48 +0300

    remove

--- js/main.js
+++ js/main.js
@@ -3,14 +3,10 @@ import { Root } from './layout'
 import { EventsList } from './events';
 import Backbone from 'backbone';
 
-const now = new Date();
-
 APP.on('start', function () {
     APP.root = new Root({
         collection: new EventsList()
     });
-
-    // const router = new Router();
     Backbone.history.start();
 });
 
