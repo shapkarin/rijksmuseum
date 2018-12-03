0d3b24fe31fddbe1f38b7231dd2d89dc73df4520
Yury Shapkarin <yu.shapkarin@gmail.com>
2018-12-04 01:32:35 +0300

    remove

--- js/main.js
+++ js/main.js
@@ -4,12 +4,10 @@ import { EventsList } from './events';
 import Backbone from 'backbone';
 
 const now = new Date();
-const today = now.toISOString().slice(0,10);
 
 APP.on('start', function () {
     APP.root = new Root({
-        collection: new EventsList(),
-        today
+        collection: new EventsList()
     });
 
     // const router = new Router();
